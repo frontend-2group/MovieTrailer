81219afe8ad73b64a2c1ddddba6099064a681375
LeeDaeGyeong <dleorud1027@gmail.com>
2023-09-25 23:46:00 +0900

    feat >> api data, homepage
    react-qurey로 데이터 받고 homePage에 포스터 이미지 조회했습니다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import HomePage from "./pages/homePage";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HomePage />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
