c3f5f5eb1493a5816bbafe043c6ebd9791a86f67
55555-Jyeon <134191817+55555-Jyeon@users.noreply.github.com>
2023-09-26 00:44:21 +0900

    fix
    App.js 충돌 해결

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import HomePage from "./pages/homePage";
-
-const queryClient = new QueryClient();
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <HomePage />
-    </QueryClientProvider>
-  );
-
-
 import "./App.css";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.style";
 import GlobalStyles from "./styles/global.style";
 import ScrollTop from "./components/scrollTop";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <ScrollTop />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <ScrollTop />
+        <HomePage />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
