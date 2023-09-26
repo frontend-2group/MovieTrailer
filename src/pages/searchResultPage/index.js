f2673b237aaa3eb27481cbbf1f0126fd23acc90a
sgm4247 <sgm4247@naver.com>
2023-09-27 00:15:32 +0900

    style >> 스타일 수정했습니다

--- src/pages/searchResultPage/index.js
+++ src/pages/searchResultPage/index.js
@@ -27,38 +27,49 @@ const SearchResultPage = () => {
   );
 };
 export default SearchResultPage;
+
 const SearchPageWapper = styled.div`
   background-color: #434343;
-  ${flexCenter} /* ${flexAlignCenter} */
+  ${flexCenter}
   height: 500px;
 `;
+
+//영화 포스터 박스
 const SearchImageBox = styled.div`
   width: 230px;
-
   img {
     width: 100%;
     border-radius: 10px;
   }
 `;
 
+//영화 제목, 간략 설명 박스
 const SearchTextBox = styled.div`
   /* color: ${({ theme }) => theme.COLORS.primary[300]}; */
+  color: ${({ theme }) => theme.COLORS.white};
   padding-left: 100px;
   line-height: 70px;
-  color: #ffffff;
+
   h1 {
     font-size: 34px;
-    font-weight: 800;
+    font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
     text-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
   }
 `;
+
+// 상세보기 버튼css
 const SearchButtonBox = styled.button`
-  background-color: #ff0458;
-  color: #ffffff;
-  border: none;
-  font-weight: 500;
-  border-radius: 30px;
-  cursor: pointer;
   width: 100px;
   height: 40px;
+  border: none;
+  border-radius: 30px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${({ theme }) => theme.COLORS.black};
+    //색상변경예정
+  }
+
+  background-color: ${({ theme }) => theme.COLORS.primary.hotPink};
+  color: ${({ theme }) => theme.COLORS.white};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
 `;
