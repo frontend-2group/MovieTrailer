312b6e103d83bc99fa3a20338268b7dd36bf3205
55555-Jyeon <134191817+55555-Jyeon@users.noreply.github.com>
2023-09-27 22:15:11 +0900

    🎁 style : header & showRelatedMovie 관련 스타일 수정
     header에 있던 input 부분을 showRelatedMovie로  따로 빼내게 되면서 스타일이 엉켜 수정했습니다.

---
+++ header.js
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+import { flexAlignCenter } from "../../styles/common.style";
+import { useNavigate } from "react-router-dom";
+import { MOVIE_QUERY_KEY } from "../../consts/movieQueryKey";
+import ShowRelatedMovie from "./showRelatedMovie";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const onOpenUpcomingMovieList = () => {
+    navigate(MOVIE_QUERY_KEY.UPCOMING);
+  };
+  const onOpenPopularMovieList = () => {
+    navigate(MOVIE_QUERY_KEY.POPULAR);
+  };
+  const onOpenTopRatedMovieList = () => {
+    navigate(MOVIE_QUERY_KEY.TOP_RATED);
+  };
+  const onOpenNowPlayingMovieList = () => {
+    navigate(MOVIE_QUERY_KEY.NOW_PLAYING);
+  };
+
+  return (
+    <HeaderWrapper>
+      <Logo>
+        <img src="/images/logo.png" alt="로고" />
+      </Logo>
+      <FilterBar>
+        <Standard onClick={onOpenUpcomingMovieList}>개봉 예정</Standard>
+        <Standard onClick={onOpenPopularMovieList}>인기 작품</Standard>
+        <Standard onClick={onOpenTopRatedMovieList}>높은 평점</Standard>
+        <Standard onClick={onOpenNowPlayingMovieList}>상영 중</Standard>
+      </FilterBar>
+      <ShowRelatedMovie />
+      <UserProfile>user</UserProfile>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
+
+const HeaderWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 80px;
+  padding: 0 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.9);
+`;
+
+const Logo = styled.div`
+  width: 100px;
+  height: 40px;
+`;
+
+const FilterBar = styled.ul`
+  position: absolute;
+  left: 16%;
+  width: 36%;
+  ${flexAlignCenter}
+  justify-content: space-evenly;
+`;
+
+const Standard = styled.li`
+  cursor: pointer;
+  color: #fff;
+
+  &:hover {
+    font-weight: bold;
+    color: #ff0458;
+  }
+`;
+
+// UserInfo.
+// 로그인/로그아웃 로직 구현 시 활용할 수 있는 회원 프로필
+const UserProfile = styled.button`
+  background-color: #ccc;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+`;
